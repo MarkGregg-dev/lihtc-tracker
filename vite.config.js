f7ee956f83a4764412762fbcf42893358ee11ff3
MarkGregg-dev <mgregg@streamlineap.com>
2026-05-01 12:24:18 -0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: fileURLToPath(new URL('./index.html', import.meta.url)),
+    },
+  },
 })
