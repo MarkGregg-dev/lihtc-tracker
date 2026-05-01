076aa9d5573ba2a0cbbf5d7c69cfc8d3168ad6e7
MarkGregg-dev <mgregg@streamlineap.com>
2026-05-01 12:36:49 -0500

    repo

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-import { fileURLToPath, URL } from 'node:url'
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: fileURLToPath(new URL('./index.html', import.meta.url)),
-    },
-  },
-=======
-
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 5c195526e68f5593939f7ab7dcf80662569b35c4
-})
+   import react from '@vitejs/plugin-react'
+   
+   export default defineConfig({
+     plugins: [react()],
+     root: '.',
+     build: {
+       outDir: 'dist',
+     },
+   })
